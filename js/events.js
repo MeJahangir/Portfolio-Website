d35890e74d56a94dac7873806c642f5923750516
jahangiralam85860 <jahangiralam85860@gmail.com>
2022-12-15 08:26:34 +0600

    submit

--- js/events.js
+++ js/events.js
@@ -1,5 +1,12 @@
-console.log('DOM er ajke special events');
+// Option 1: directly set on the html element
+//<button onclick="console.log(10)">Click Me!</button> 
 
+// Option 2: add  onclick function on the html element
+//<button onclick="makeRed()">Make red</button>
 function makeRed() {
     document.body.style.backgroundColor="red";
 }
+
+// Option 3: another
+const makeBlueButton = decument.getElementById('Make-blue');
+makeBlueButton.onclick = MakeBlue;

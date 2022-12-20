6eb527e5040f9449bc6ec9b5d6fd230e2516a01b
jahangiralam85860 <jahangiralam85860@gmail.com>
2022-12-21 00:11:45 +0600

    onclick button add

--- js/lastEvent.js
+++ js/lastEvent.js
@@ -1,1 +1,9 @@
-console.log('hello bangladesh');
+function handleOnClick() {
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = 'Text updated by add event Listener';
+}
+
+document.getElementById('event-listner').addEventListener('click', function () {
+    const handlerStatus = document.getElementById('handler-status');
+    handlerStatus.innerText = "MD JAHANGIR ALAM JOY";
+})
